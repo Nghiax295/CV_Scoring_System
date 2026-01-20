6621dbd67ab719015645a7e74e1fe39b81460476
Nguyễn Nhật Dũng Lân <102230302@sv1.dut.udn.vn>
2026-01-20 17:04:36 +0700

    Add accounts app with custom User model

---
+++ accounts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('recruiter', 'Recruiter'),
+        ('candidate', 'Candidate'),
+    )
+    
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES,
+        default='candidate'
+    )
